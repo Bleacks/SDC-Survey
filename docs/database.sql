9c1a556bdd81e9c4c811a9b50b7605e368497752
Maxime Dolet <maxime.dolet8@etu.univ-lorraine.fr>
2017-06-09 11:35:11 +0200

    Website's database creation script

---
+++ docs/database.sql
@@ -0,0 +1,110 @@
+DROP TABLE IF EXISTS CollecRole;
+DROP TABLE IF EXISTS Role;
+DROP TABLE IF EXISTS UserTool;
+DROP TABLE IF EXISTS CollecTool;
+DROP TABLE IF EXISTS PersoTool;
+DROP TABLE IF EXISTS Tool;
+DROP TABLE IF EXISTS SCollec;
+DROP TABLE IF EXISTS SPerso;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Groups;
+
+CREATE TABLE Role (
+   RoleId	int(3) AUTO_INCREMENT,
+   RoleName varchar(50) NOT NULL,
+   PRIMARY KEY (RoleId)
+);
+
+CREATE TABLE Groups (
+   GroupId    int(6) AUTO_INCREMENT,
+   GroupName  VARCHAR(30) NOT NULL,
+   GroupImage VARCHAR(100),
+   PRIMARY KEY (GroupId)
+);
+
+CREATE TABLE Users (
+   UserId      int(6) AUTO_INCREMENT,
+   GroupId     int(6),
+   FirstName	VARCHAR(30),
+   LastName	   VARCHAR(30),
+   Mail        VARCHAR(40),
+   Pass        VARCHAR(50), -- // TODO: Changer la longueur du varchar à celle du hash
+   Admin       boolean,
+   MemberSince	Date,
+   PRIMARY KEY (UserId),
+   CONSTRAINT fk_user_groupid FOREIGN KEY (GroupId)
+   REFERENCES Groups(GroupId)
+);
+
+CREATE TABLE SCollec (
+   SurvId      int(6) AUTO_INCREMENT,
+   UserId      int(6),
+   UseTime     int(6),
+   StartDate   Date,
+   FinishDate  Date,
+   DocId       int(6),
+   Context     int(1),        -- Time and Place : 4 Option (both same or different)
+   PRIMARY KEY (SurvId),
+   CONSTRAINT fk_scollec_userid FOREIGN KEY (UserId)
+   REFERENCES Users(UserId)
+);
+
+CREATE TABLE SPerso (
+   SurvId      int(6) AUTO_INCREMENT,
+   UserId      int(6),
+   UseTime     int(6),
+   StartDate   Date,
+   FinishDate  Date,
+   DocId       int(6),
+   PRIMARY KEY (SurvId),
+   CONSTRAINT fk_sperso_userid FOREIGN KEY (UserId)
+   REFERENCES Users(UserId)
+);
+
+CREATE TABLE CollecRole (
+   SurvId      int(6),
+   RoleId      int(6),
+   NbPeople    int(6),
+   PRIMARY KEY (SurvId, RoleId),
+   CONSTRAINT fk_collecrole_survid FOREIGN KEY (SurvId)
+   REFERENCES SCollec(SurvId),
+   CONSTRAINT fk_collecrole_roleid FOREIGN KEY (RoleId)
+   REFERENCES Role(RoleId)
+);
+
+CREATE TABLE Tool (
+   ToolId      int(6) AUTO_INCREMENT,
+   ToolName    VARCHAR(40),
+   Category    int(2),           -- Category defines if : Tool / Focus / Activity / Abstraction
+   PRIMARY KEY (ToolId)
+);
+
+CREATE TABLE UserTool (
+   UserId      int(6),
+   ToolId      int(6),
+   PRIMARY KEY (UserID, ToolId),
+   CONSTRAINT fk_usertool_userid FOREIGN KEY (UserId)
+   REFERENCES Users(UserId),
+   CONSTRAINT fk_usertool_toolid FOREIGN KEY (ToolId)
+   REFERENCES Tool(ToolId)
+);
+
+CREATE TABLE CollecTool (
+   SurvId      int(6),
+   ToolId      int(6),
+   PRIMARY KEY (SurvId, ToolId),
+   CONSTRAINT fk_collectool_survid FOREIGN KEY (SurvId)
+   REFERENCES SCollec(SurvId),
+   CONSTRAINT fk_collectool_toolid FOREIGN KEY (ToolId)
+   REFERENCES Tool(ToolId)
+);
+
+CREATE TABLE PersoTool (
+   SurvId      int(6),
+   ToolId      int(6),
+   PRIMARY KEY (SurvId, ToolId),
+   CONSTRAINT fk_persotool_survid FOREIGN KEY (SurvId)
+   REFERENCES SPerso(SurvId),
+   CONSTRAINT fk_persotool_toolid FOREIGN KEY (ToolId)
+   REFERENCES Tool(ToolId)
+);
